79bf8593fba8db2e14e6b8b700b8403d84213aa2
Hector Martinez <hec.gre81@gmail.com>
2024-04-14 16:23:27 -0600

    Add printing from prompts

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,28 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 import { getPrompts } from "./services/getPrompts";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [prompts, setPrompts] = useState<null | Prompt[]>(null);
 
-  getPrompts()
-    .then((prompts) => {
-      console.log("Prompts fetched");
-      console.log(prompts);
-    })
-    .catch((error) => console.error("Error fetching prompts:", error));
+  useEffect(() => {
+    getPrompts()
+      .then((prompts) => {
+        console.log("Prompts fetched");
+        setPrompts(prompts);
+      })
+      .catch((error) => console.error("Error fetching prompts:", error));
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {prompts?.map((prompt) => (
+        <section key={prompt.createTime.seconds}>
+          <h2>You: {prompt.prompt}</h2>
+          <p>Response: {prompt.response}</p>
+          <hr />
+        </section>
+      ))}
     </>
   );
 }
