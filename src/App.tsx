6e2369143b7740886060d4e8fcf9579f361bfb66
Hector Martinez <hec.gre81@gmail.com>
2024-05-01 19:39:56 -0600

    Add a initial prompt

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,9 @@ import { getPrompts } from "./services/getPrompts";
 import styles from "./Chat.module.css";
 import useSessionId from "./hooks/useSessionId";
 
+const INITIAL_PROMPT =
+  "¡Hola! ¿En qué puedo ayudarte hoy? Estaré encantado de buscar en nuestra base de datos para encontrar una mascota que coincida con tus criterios. ¿Qué tipo de mascota estás buscando? Si no estás seguro, estaré encantado de guiarte a través de algunas preguntas para ayudarte a reducir tus opciones";
+
 function App() {
   const [prompts, setPrompts] = useState<null | Prompt[]>(null);
   const sessionId = useSessionId();
@@ -23,6 +26,14 @@ function App() {
 
   return (
     <>
+      <main>
+        <main></main>
+        <main className={styles.chat__container}>
+          <section className={styles.chat__assistant}>
+            <p>{INITIAL_PROMPT}</p>
+          </section>
+        </main>
+      </main>
       {prompts?.map((prompt) => (
         <main key={prompt.createTime.seconds}>
           <main className={styles.chat__container}>
