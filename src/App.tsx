702fdbfc2afe025e60e9fdd113a858f10c5cbdb9
Hector Martinez <hec.gre81@gmail.com>
2024-05-12 14:41:05 -0600

    Add chatbot with pages, this works like previous one but has the implementation on routes folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,7 @@
-import { useState, useEffect } from "react";
+import Chat from "routes/chat/index";
 import "./App.css";
-import Input from "./components/Input";
-import { getPrompts } from "./services/getPrompts";
-import styles from "./Chat.module.css";
-import useSessionId from "./hooks/useSessionId";
-
-const INITIAL_PROMPT =
-  "¡Hola! ¿En qué puedo ayudarte hoy? Estaré encantado de buscar en nuestra base de datos para encontrar una mascota que coincida con tus criterios. ¿Qué tipo de mascota estás buscando? Si no estás seguro, estaré encantado de guiarte a través de algunas preguntas para ayudarte a reducir tus opciones";
-
 function App() {
-  const [prompts, setPrompts] = useState<null | Prompt[]>(null);
-  const sessionId = useSessionId();
-  if (!sessionId.id) sessionId.setSessionId();
-
-  useEffect(() => {
-    getPrompts({
-      sessionId: sessionId.id,
-    })
-      .then((prompts) => {
-        console.log("Prompts fetched");
-        setPrompts(prompts);
-      })
-      .catch((error) => console.error("Error fetching prompts:", error));
-  }, [sessionId.id]);
-
-  return (
-    <>
-      <main>
-        <main></main>
-        <main className={styles.chat__container}>
-          <section className={styles.chat__assistant}>
-            <p>{INITIAL_PROMPT}</p>
-          </section>
-        </main>
-      </main>
-      {prompts?.map((prompt) => (
-        <main key={prompt.createTime.seconds}>
-          <main className={styles.chat__container}>
-            <section className={styles.chat__mine}>
-              <p>{prompt.promptToDisplay}</p>
-            </section>
-          </main>
-          <main className={styles.chat__container}>
-            <section className={styles.chat__assistant}>
-              <p>{prompt.response}</p>
-            </section>
-          </main>
-        </main>
-      ))}
-      <Input />
-    </>
-  );
+  return <Chat />;
 }
 
 export default App;
