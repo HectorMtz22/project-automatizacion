d3ffb63eaba8d43112b5d2f5f377d8d77ebd5ad9
Hector Martinez <hec.gre81@gmail.com>
2024-05-12 14:45:27 -0600

    Add /chat page on router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
-import Chat from "routes/chat/index";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
 import "./App.css";
+import Chat from "routes/chat/index";
+
+const router = createBrowserRouter([
+  {
+    path: "/chat",
+    element: <Chat />,
+  },
+]);
+
 function App() {
-  return <Chat />;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
